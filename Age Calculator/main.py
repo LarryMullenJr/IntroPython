8d82465f145826c5c7cf4c74cf93bda9fcf8878d
LarryMullenJr <mdumouc2@uwo.ca>
2024-05-12 17:00:22 -0400

    Add files via upload
    Mini Project - Calculate Age

---
+++ Age Calculator/main.py
@@ -0,0 +1,13 @@
+# Take input from the user for start_year
+start_year = int(input("Enter the start year: "))
+
+# Take input from the user for end_year
+end_year = int(input("Enter the end year: "))
+
+# Calculate the age
+age = end_year - start_year
+
+# Print Results
+print("Start Year:", start_year)
+print("End Year:", end_year)
+print("Calculated Age:", age)
