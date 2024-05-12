ac5dd0f799d8f1a1e813200b59c29d088a817ddd
LarryMullenJr <mdumouc2@uwo.ca>
2024-05-12 17:40:39 -0400

    Add files via upload

---
+++ Calculate Circle/main.py
@@ -0,0 +1,17 @@
+# Take input from the user for the radius
+radius = int(input("Enter the radius of the circle: "))
+
+# Print the Radius
+print("The radius of your circle is:", radius)
+
+# Calculate the circumference
+circumference = radius * 2 * 3.14159
+
+# Calculate the area
+area = 3.14159 * radius ** 2
+
+# Print the circumference
+print("The circumference of your circle is:", circumference)
+
+# Print the area
+print("The area of your circle is:", area)
