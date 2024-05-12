f42d663d4d56679a4888e9b5ab678f4b7b1d38b2
LarryMullenJr <mdumouc2@uwo.ca>
2024-05-12 17:20:19 -0400

    Add files via upload

---
+++ Calculator/main.py
@@ -0,0 +1,33 @@
+# Take input from the user for first_number
+first_number = int(input("Enter the first number: "))
+
+# Take input from the user for second_year
+second_number = int(input("Enter the second number: "))
+
+# Print the inputs
+print("First number:", first_number)
+print("Second number:", second_number)
+
+# Calculate the sum of the two numbers
+sum = first_number + second_number
+
+# Calculate the subtraction of the two numbers
+subtraction = first_number - second_number
+
+# Calculate the multiplication of the two numbers
+multiplication = first_number * second_number
+
+# Calculate the division of the two numbers
+division = first_number / second_number
+
+# Print the sum
+print("Sum of the two numbers:", sum)
+
+# Print the subtraction
+print("Subtraction of second number from first number:", subtraction)
+
+# Print the multiplication
+print("Multiplication of the two numbers:", multiplication)
+
+# Print the division
+print("Division of the second number by the first number:", division)
